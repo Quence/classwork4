56e3226a37fc2458e54e92df450fab2295c25ad8
AP <montik91@gmail.com>
2023-09-30 08:11:04 +0000

    push

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var imgObj = null;
+            
+            function init() {
+               imgObj = document.getElementById('myImage');
+               imgObj.style.position= 'relative'; 
+               imgObj.style.left = '0px'; 
+            }
+            function moveRight() {
+               imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
+            }
+            
+            window.onload = init;
+
+document.querySelector('.submit-email').addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    document.querySelector('.subscription').classList.add('done');
+  });
